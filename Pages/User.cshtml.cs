ca50c48b35c0ec84dbc3b5ea8f5264f84a62005c
Jessica Heller <jlheller1@buffs.wtamu.edu>
2023-05-08 20:40:48 -0500

    changes

--- Pages/User.cshtml.cs
+++ Pages/User.cshtml.cs
@@ -4,49 +4,35 @@ using Microsoft.EntityFrameworkCore;
 using Final_Project.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Final_Project.Pages
+namespace Final_Project.Pages;
+
+public class UserModel : PageModel
 {
-    public class UserModel : PageModel
+    private readonly ProjectDbContext _context;
+    private readonly ILogger<UserModel> _logger;
+    public List<User> Users {get; set;} = default!;
+    public SelectList UserDropDown {get; set;} = default!;
+
+    [BindProperty]
+    public new User User {get; set;} = default!;
+
+    public UserModel (ProjectDbContext context, ILogger<UserModel> logger)
     {
-        private readonly ProjectDbContext _context;
-        private readonly ILogger<UserModel> _logger;
-        public List<User> Users { get; set; } = default!;
-        public SelectList UserDropDown { get; set; } = default!;
+        _context = context; 
+        _logger = logger;
+    }
 
-        [BindProperty]
-        public new User User { get; set; } = default!;
+    public void OnGet()
+    {
+        Users = _context.User.ToList();
+        UserDropDown = new SelectList(Users, "UserId", "FirstName");
+    }
 
-        public UserModel(ProjectDbContext context, ILogger<UserModel> logger)
-        {
-            _context = context;
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-            Users = _context.User.ToList();
-            UserDropDown = new SelectList(Users, "UserId", "FirstName");
-        }
-
-        public IActionResult OnPost()
-        {
-            if (User.UserId == 0)
-            {
-                if (!ModelState.IsValid)
-                {
-                    return Page();
-                }
-
-                _context.User.Add(User);
-                _context.SaveChanges();
-            }
-
-            User = _context.User.Include(u => u.UserBooks).ThenInclude(ub => ub.Book).FirstOrDefault(u => u.UserId == User.UserId);
-            Users = _context.User.ToList();
-            UserDropDown = new SelectList(Users, "UserId", "FirstName", "LastName");
-
-            return Page();
-        }
+    public void OnPost()
+    {
+       
+        User = _context.User.Include(u => u.UserBooks).ThenInclude(ub => ub.Book).Where(u => u.UserId == User.UserId).First();
+        Users = _context.User.ToList();
+        UserDropDown = new SelectList(Users, "UserId", "FirstName", "LastName");
     }
 }
-
